cb7cb5c3d995a0e72b7a3740cd76b507f689521c
Aakash Bhumbla <aakash.bhumbla@gmail.com>
2022-03-29 04:32:03 -0500

    FEAT: updates graphql root schema

--- server/schema/types/root_query_type.js
+++ server/schema/types/root_query_type.js
@@ -1,8 +1,19 @@
 const graphql = require('graphql');
-const { GraphQLObjectType } = graphql;
+const { GraphQLObjectType, GraphQLString } = graphql;
+const _ = require('lodash');
+const UserType = require('./user_type');
 
 const RootQueryType = new GraphQLObjectType({
-  name: 'RootQueryType'
+  name: 'RootQueryType',
+  fields: {
+    currentUser: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args, req) {
+        return req.user;
+      },
+    },
+  },
 });
 
 module.exports = RootQueryType;
--- server/schema/types/user_type.js
+++ server/schema/types/user_type.js
@@ -1,10 +1,10 @@
 const graphql = require('graphql');
-const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLID } = graphql;
 
 const UserType = new GraphQLObjectType({
   name: 'UserType',
   fields: {
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
     email: { type: GraphQLString },
